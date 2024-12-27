40b44dfa2db5e066c6c39b2acfb3b0b197382352
Rory Collins <rory.collins@hotmail.co.uk>
2024-12-27 11:11:15 +0000

    Day 15 part 2

--- 2024.Day15/Solution.cs
+++ 2024.Day15/Solution.cs
@@ -11,24 +11,27 @@ public class Solution
     private Coordinate2D start;
     private List<Coordinate2D> boulders;
     private IEnumerable<Coordinate2D> walls;
-    private IEnumerable<Direction> instructions;
-    
+    private IEnumerable<Coordinate2D> instructions;
+    private IEnumerable<(Coordinate2D, Coordinate2D)> newBoulders;
+    private IEnumerable<Coordinate2D> newWalls;
+
     public Solution(string testInput, string input)
     {
         var parts = (UseTestInput ? testInput : input).Split("\n\n");
         grid = new Grid<char>(parts[0].Split("\n"));
+
         instructions = parts[1].ReplaceLineEndings("").Select(c =>
         {
             return c switch
             {
-                'v' => Direction.North,
-                '>' => Direction.East,
-                '<' => Direction.West,
-                '^' => Direction.South,
+                'v' => new Coordinate2D(0, 1),
+                '>' => new Coordinate2D(1, 0),
+                '<' => new Coordinate2D(-1, 0),
+                '^' => new Coordinate2D(0, -1),
                 _ => throw new ArgumentException($"Invalid direction: {c}")
             };
         });
-        
+
         boulders = grid.FindAll('O').ToList();
         walls = grid.FindAll('#');
         start = grid.FindAll('@').Single();
@@ -36,55 +39,159 @@ public class Solution
 
     public object PartOne()
     {
-        var end = instructions.Aggregate(start, (c, instruction) => DoIt(instruction, c));
-        return boulders.Sum(it => it.Y * 100 + it.X);
-    }
-    public object PartTwo()
-    {
-        return "Not yet implemented";
+        var state = instructions.Aggregate(new GridStatePartOne(start, boulders, walls),
+            (c, instruction) => ProcessInstructionPartOne(instruction, c));
+
+        return state.Boulders.Sum(it => it.Y * 100 + it.X);
     }
 
-    private Coordinate2D DoIt(Direction direction, Coordinate2D current)
+    public object PartTwo()
     {
-        var proposal = current.Move(direction);
+        newBoulders = boulders.Select(c => (c with { X = c.X * 2 }, c with { X = c.X * 2 + 1 }));
+        newWalls = walls.SelectMany(c => new[] { c with { X = c.X * 2 }, c with { X = c.X * 2 + 1 } });
+        var newStart = start with { X = start.X * 2 };
+
+        var state = instructions.Aggregate(new GridStatePartTwo(newStart, newBoulders, newWalls),
+            (c, instruction) => ProcessInstructionPartTwo(instruction, c));
+
+        return state.Boulders.Sum(it => it.Left.Y * 100 + it.Left.X);
+    }
+
+    private record GridStatePartOne(
+        Coordinate2D Robot,
+        IEnumerable<Coordinate2D> Boulders,
+        IEnumerable<Coordinate2D> Walls);
+
+    private static GridStatePartOne ProcessInstructionPartOne(Coordinate2D direction, GridStatePartOne state)
+    {
+        var proposal = state.Robot.Add(direction);
+        var walls = state.Walls.ToList();
+        var boulders = state.Boulders.ToList();
+
         if (walls.Contains(proposal))
         {
-            return current;
+            return state;
         }
 
         if (boulders.Contains(proposal))
         {
-            var boulderTarget = proposal.Move(direction);
+            var boulderTarget = proposal.Add(direction);
             while (boulders.Contains(boulderTarget))
             {
-                boulderTarget = boulderTarget.Move(direction);
+                boulderTarget = boulderTarget.Add(direction);
             }
+
             if (walls.Contains(boulderTarget))
             {
-                return current;
+                return state;
             }
+
             boulders.Remove(proposal);
             boulders.Add(boulderTarget);
-            return proposal;
         }
-        return proposal;
+
+        return state with { Boulders = boulders, Robot = proposal };
     }
 
+    private record GridStatePartTwo(
+        Coordinate2D Robot,
+        IEnumerable<(Coordinate2D Left, Coordinate2D Right)> Boulders,
+        IEnumerable<Coordinate2D> Walls);
 
-    private void PrintGrid(Coordinate2D end)
+    private static GridStatePartTwo ProcessInstructionPartTwo(Coordinate2D direction, GridStatePartTwo state)
     {
-        for (int y = 0; y < grid.Height; y++)
+        var proposal = state.Robot.Add(direction);
+        var walls = state.Walls.ToList();
+
+        if (walls.Contains(proposal))
         {
-            for (int x = 0; x < grid.Width; x++)
+            return state;
+        }
+
+        var boulders = state.Boulders.ToList();
+
+        // boulders.Where(b => new[] { b.Left, b.Right }.Contains(proposal));
+
+        var bouldersToMove = new HashSet<(Coordinate2D, Coordinate2D)>();
+
+        var queue = new System.Collections.Generic.Queue<Coordinate2D>();
+
+        var boulderEncountered = boulders.FirstOrDefault(b => new[] { b.Left, b.Right }.Contains(proposal));
+        if (boulderEncountered != default)
+        {
+            bouldersToMove.Add(boulderEncountered);
+            queue.Enqueue(boulderEncountered.Left);
+            queue.Enqueue(boulderEncountered.Right);
+        }
+
+        while (queue.Count > 0)
+        {
+            var p = queue.Dequeue();
+            var target = p.Add(direction);
+            if (walls.Contains(target))
             {
-                var current = new Coordinate2D(x, y);
-                var symbol = current == end
+                return state;
+            }
+
+            var found = boulders.FirstOrDefault(b => new[] { b.Left, b.Right }.Contains(target));
+            if (found != default)
+            {
+                var added = bouldersToMove.Add(found);
+                if (added)
+                {
+                    queue.Enqueue(found.Left);
+                    queue.Enqueue(found.Right);
+                }
+            }
+        }
+
+
+        foreach (var boulder in bouldersToMove)
+        {
+            boulders.Remove(boulder);
+            boulders.Add((boulder.Item1.Add(direction), boulder.Item2.Add(direction)));
+        }
+
+        return state with { Boulders = boulders, Robot = proposal };
+    }
+
+    private static void PrintGrid(GridStatePartOne state)
+    {
+        for (int y = 0; y <= state.Walls.Max(it => it.Y); y++)
+        {
+            for (int x = 0; x <= state.Walls.Max(it => it.X); x++)
+            {
+                var cell = new Coordinate2D(x, y);
+                var symbol = cell == state.Robot
                     ? '@'
-                    : walls.Contains(current)
+                    : state.Walls.Contains(cell)
                         ? '#'
-                        : boulders.Contains(current)
+                        : state.Boulders.Contains(cell)
                             ? 'O'
                             : '.';
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private static void PrintGrid(GridStatePartTwo state)
+    {
+        for (int y = 0; y <= state.Walls.Max(it => it.Y); y++)
+        {
+            for (int x = 0; x <= state.Walls.Max(it => it.X); x++)
+            {
+                var cell = new Coordinate2D(x, y);
+                var symbol = cell == state.Robot
+                    ? '@'
+                    : state.Walls.Contains(cell)
+                        ? '#'
+                        : state.Boulders.Select(it => it.Item1).Contains(cell)
+                            ? '['
+                            : state.Boulders.Select(it => it.Item2).Contains(cell)
+                                ? ']'
+                                : '.';
                 Console.Write(symbol);
             }
 
