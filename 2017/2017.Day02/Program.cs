3b0ca18e26cbb1caa44f101e47860eb897c109f6
Rory Collins <rory.collins@hotmail.co.uk>
2025-11-19 13:57:07 +0000

    Add project files for Days 1 to 16 and update .gitignore for input files

---
+++ 2017/2017.Day02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using _2017.Day02;
+
+var testInput = File.ReadAllLines("testInput.txt")
+    .ToList();
+var input = File.ReadAllLines("input.txt")
+    .ToList();
+var solution = new Solution(testInput, input);
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+Console.WriteLine($"Part one: {solution.PartOne()}");
+stopwatch.Stop();
+var partOneSpeed = stopwatch.ElapsedMilliseconds;
+stopwatch.Restart();
+Console.WriteLine($"Part two: {solution.PartTwo()}");
+stopwatch.Stop();
+var partTwoSpeed = stopwatch.ElapsedMilliseconds;
+
+Console.WriteLine();
+Console.WriteLine($"Part one took {partOneSpeed}ms");
+Console.WriteLine($"Part two took {partTwoSpeed}ms");
---
+++ 2024/2024.Day13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using _2024.Day13;
+
+var testInput = File.ReadAllText("testInput.txt");
+var input = File.ReadAllText("input.txt");
+var solution = new Solution(testInput, input);
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+Console.WriteLine($"Part one: {solution.PartOne()}");
+stopwatch.Stop();
+var partOneSpeed = stopwatch.ElapsedMilliseconds;
+stopwatch.Restart();
+Console.WriteLine($"Part two: {solution.PartTwo()}");
+stopwatch.Stop();
+var partTwoSpeed = stopwatch.ElapsedMilliseconds;
+
+Console.WriteLine();
+Console.WriteLine($"Part one took {partOneSpeed}ms");
+Console.WriteLine($"Part two took {partTwoSpeed}ms");
