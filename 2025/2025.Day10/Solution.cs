dde7c490ae98d787f9fb8820dfc2b63645b6bf86
Rory Collins <rory.collins@hotmail.co.uk>
2026-01-06 09:07:28 +0000

    Day 10 Part 2 WIP

--- 2025/2025.Day10/Solution.cs
+++ 2025/2025.Day10/Solution.cs
@@ -5,30 +5,73 @@ namespace _2025.Day10;
 
 public partial class Solution
 {
-    private record Machine(int Target, IEnumerable<int> Buttons)
+    private class Machine(string lightTarget, IEnumerable<int[]> buttons, int[] joltageTarget)
     {
-        public override string ToString()
-        {
-            return $"{Convert.ToString(Target, 2)} | {string.Join(" ~ ", Buttons.Select(b => Convert.ToString(b, 2)))}";
-        }
+        private readonly IEnumerable<int> buttonsForLights = buttons
+            .Select(b =>
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < lightTarget.Length; i++)
+                {
+                    sb.Append(b.Contains(i) ? '1' : '0');
+                }
+                return sb.ToString();
+            })
+            .Select(s => Convert.ToInt32(s, 2));
 
-        public int ButtonPressesToTarget()
+
+        public int ButtonPressesToReachLightTarget()
         {
+            var targetDiagram = lightTarget
+                .Replace('.', '0')
+                .Replace('#', '1');
+            var targetAsNumber = Convert.ToInt32(targetDiagram, 2);
+
             List<int> current = [0];
             var buttonPresses = 0;
-            while (!current.Contains(Target))
+            while (!current.Contains(targetAsNumber))
             {
                 buttonPresses++;
-                current = current.SelectMany(this.OneButtonPress).ToList();
+                current = current.SelectMany(this.PressButtonForLights).ToList();
             }
             return buttonPresses;
         }
 
-        public IEnumerable<int> OneButtonPress(int current)
+        public int ButtonPressesToReachJoltageTarget()
         {
-            return this.Buttons.Select(button => current ^ button);
+            var buttonPresses = 0;
+            List<int[]> current = [new int[joltageTarget.Length]];
+            while (!current.Any(j => Enumerable.SequenceEqual(j, joltageTarget)))
+            {
+                buttonPresses++;
+                var intsList = current.SelectMany(this.PressButtonForJoltage).Where(x => x.Length > 0).ToList();
+                var foo = intsList.DistinctBy(x => string.Join("", x)).ToList();
+                current = foo;
+            }
+            return buttonPresses;
         }
 
+        private IEnumerable<int> PressButtonForLights(int current)
+        {
+            return this.buttonsForLights.Select(button => current ^ button);
+        }
+
+        private IEnumerable<int[]> PressButtonForJoltage(int[] current)
+        {
+            return buttons.Select(elementsToIncrement =>
+            {
+                var candidate = current.ToArray();
+                foreach (var i in elementsToIncrement)
+                {
+                    candidate[i]++;
+                }
+                return this.IsValid(candidate) ? candidate : [];
+            });
+        }
+        private bool IsValid(int[] candidate)
+        {
+            return !candidate.Where((t, i) => t > joltageTarget[i]).Any();
+        }
     }
 
 
@@ -40,43 +83,38 @@ public partial class Solution
     {
         this.machines = (UseTestInput ? testInput : input).Select(line =>
             {
-                var value = TargetDiagramRegex().Match(line).Groups[1].Value;
-                var targetDiagram = value
-                    .Replace('.', '0')
-                    .Replace('#', '1');
-                var targetAsNumber = Convert.ToInt32(targetDiagram, 2);
+                var targetLightDiagram = TargetDiagramRegex().Match(line).Groups[1].Value;
 
                 var buttons = ButtonRegex().Matches(line)
                     .Select(match => match.Groups[1].Value)
-                    .Select(s => s.Split(',').Select(int.Parse).ToArray())
-                    .Select(b =>
-                    {
-                        var sb = new StringBuilder();
-                        for (int i = 0; i < targetDiagram.Length; i++)
-                        {
-                            sb.Append(b.Contains(i) ? '1' : '0');
-                        }
-                        return sb.ToString();
-                    })
-                    .Select(s => Convert.ToInt32(s, 2));
+                    .Select(s => s.Split(',').Select(int.Parse).ToArray());
+                
+                var joltages = JoltageRegex().Match(line).Groups[1].Value
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
 
-                return new Machine(targetAsNumber, buttons);
+                return new Machine(targetLightDiagram, buttons, joltages);
             }
         );
     }
 
     public object PartOne()
     {
-        return this.machines.Select(m => m.ButtonPressesToTarget()).Sum();
+        return this.machines.Select(m => m.ButtonPressesToReachLightTarget()).Sum();
     }
 
     public object PartTwo()
     {
-        return "Not yet implemented";
+        return this.machines.Select(m => m.ButtonPressesToReachJoltageTarget()).Sum();
     }
 
     [GeneratedRegex(@"\[(.*)\]")]
     private static partial Regex TargetDiagramRegex();
+    
     [GeneratedRegex(@"\((.*?)\)")]
     private static partial Regex ButtonRegex();
+    
+    [GeneratedRegex(@"\{(.*?)\}")]
+    private static partial Regex JoltageRegex();
 }
