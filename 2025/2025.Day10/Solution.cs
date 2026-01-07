3654ecb4540a5bba7e022d217b9b9b00ab1033a0
Rory Collins <rory.collins@hotmail.co.uk>
2026-01-07 16:44:14 +0000

    Day Ten part two wip

--- 2025/2025.Day10/Solution.cs
+++ 2025/2025.Day10/Solution.cs
@@ -5,6 +5,7 @@ namespace _2025.Day10;
 
 public partial class Solution
 {
+
     private class Machine(string lightTarget, IEnumerable<int[]> buttons, int[] joltageTarget)
     {
         private readonly IEnumerable<int> buttonsForLights = buttons
@@ -39,38 +40,117 @@ public partial class Solution
 
         public int ButtonPressesToReachJoltageTarget()
         {
-            var buttonPresses = 0;
-            List<int[]> current = [new int[joltageTarget.Length]];
-            while (!current.Any(j => Enumerable.SequenceEqual(j, joltageTarget)))
+
+            //translate buttons to matrix in Row Echelon Form
+            var rows = joltageTarget.Length;
+            var cols = buttons.Count()+1;
+            var buttonsArr = buttons.ToArray();
+            var m = new List<int[]>();
+            for (int y = 0; y < rows; y++)
             {
-                buttonPresses++;
-                var intsList = current.SelectMany(this.PressButtonForJoltage).Where(x => x.Length > 0).ToList();
-                var foo = intsList.DistinctBy(x => string.Join("", x)).ToList();
-                current = foo;
+                var r = new List<int>();
+                for (int x = 0; x < cols-1; x++)
+                {
+                    r.Add(buttonsArr[x].Contains(y) ? 1 : 0);
+                }
+                r.Add(joltageTarget[y]);
+                m.Add(r.ToArray());
             }
-            return buttonPresses;
+
+            var matrix = m.ToArray().OrderByDescending(x => int.Parse(string.Join("", x[..^1]))).ToArray();
+
+            // identify remaining free variables
+            var freeVariables= Enumerable.Range(0, buttons.Count()).ToList();
+            var buttonCounter = 0;
+            foreach (var row in matrix)
+            {
+                if (buttonCounter > 0 && row[..buttonCounter].Any(x => x != 0))
+                {
+                    continue;
+                }
+                while (buttonCounter < buttons.Count())
+                {
+                    if (row[buttonCounter] == 1)
+                    {
+                        freeVariables.Remove(buttonCounter);
+                        buttonCounter++;
+                        break;
+                    }
+                    buttonCounter++;
+                }
+            }
+
+            // find their max values
+            var freeVariableValues = freeVariables
+                .Select(i => (i, matrix
+                    .Where(r => r[i] == 1)
+                    .Select(r => r.Last()).ToArray()))
+                .Select(fv => (fv.i, fv.Item2.Min()))
+                .ToDictionary();
+
+            matrix = matrix.OrderBy(x => int.Parse(string.Join("", x[..^1]))).ToArray();
+
+            return DoItRx(matrix, freeVariableValues, new Dictionary<int, int>());
+        }
+
+        private static int DoItRx(int[][] matrix, Dictionary<int, int> freeVariableValues, Dictionary<int, int> currentValues)
+        {
+            if (freeVariableValues.Count == 0) return DoIt(matrix, currentValues);
+
+            var (ix, max) = freeVariableValues.First();
+            var results = Enumerable.Range(0, max).Select(r =>
+            {
+                var foo = freeVariableValues.ToDictionary();
+                var nextVals = currentValues.ToDictionary();
+                nextVals.Add(ix, r);
+                foo.Remove(ix);
+                return DoItRx(matrix, foo, nextVals);
+            });
+
+            return results.Min();
+        }
+
+        private static int DoIt(int[][] matrix, Dictionary<int, int> currentValues)
+        {
+            var valid = true;
+            foreach (var row in matrix)
+            {
+                var sln = row[^1];
+                var unknown = -1;
+                for (var i = 0; i < row.Length - 1; i++)
+                {
+                    if (row[i] == 0) continue;
+
+                    if (!currentValues.TryGetValue(i, out var value))
+                    {
+                        unknown = i;
+                    };
+
+                    sln -= value;
+                    if (sln < 0)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                switch (unknown)
+                {
+                    case -1 when sln != 0:
+                        valid = false;
+                        break;
+
+                    case >= 0:
+                        currentValues[unknown] = sln;
+                        break;
+                }
+                if (!valid) { break; }
+            }
+            return valid ? currentValues.Sum(it => it.Value) : int.MaxValue;
         }
 
         private IEnumerable<int> PressButtonForLights(int current)
         {
             return this.buttonsForLights.Select(button => current ^ button);
-        }
-
-        private IEnumerable<int[]> PressButtonForJoltage(int[] current)
-        {
-            return buttons.Select(elementsToIncrement =>
-            {
-                var candidate = current.ToArray();
-                foreach (var i in elementsToIncrement)
-                {
-                    candidate[i]++;
-                }
-                return this.IsValid(candidate) ? candidate : [];
-            });
-        }
-        private bool IsValid(int[] candidate)
-        {
-            return !candidate.Where((t, i) => t > joltageTarget[i]).Any();
         }
     }
 
@@ -101,12 +181,13 @@ public partial class Solution
 
     public object PartOne()
     {
+        return "skipped";
         return this.machines.Select(m => m.ButtonPressesToReachLightTarget()).Sum();
     }
 
     public object PartTwo()
     {
-        return this.machines.Select(m => m.ButtonPressesToReachJoltageTarget()).Sum();
+        return this.machines.Select(m => m.ButtonPressesToReachJoltageTarget()).Select(s => (long)s).Sum();
     }
 
     [GeneratedRegex(@"\[(.*)\]")]
